1c2b63ebe597e80105057c914c54bee989a334cf
Anurag Yadav <anuragyadav622003@gmail.com>
2025-11-12 16:31:29 +0530

    frontend

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 // frontend/src/lib/api.ts
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api'
+const API_BASE_URL =  'http://localhost:3000'
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
