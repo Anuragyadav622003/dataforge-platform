41ed1364c28b1b69570df546deec84ae52c52d7f
Anurag Yadav <anuragyadav622003@gmail.com>
2025-11-13 21:15:26 +0530

    backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,66 +1,35 @@
-// // backend/src/main.ts
-// import { NestFactory } from '@nestjs/core';
-// import { ValidationPipe } from '@nestjs/common';
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-// import cors from 'cors'
-// import { AppModule } from './app.module';
-
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-  
-//   app.useGlobalPipes(new ValidationPipe({
-//     whitelist: true,
-//     forbidNonWhitelisted: true,
-//   }));
-
-//     app.enableCors({
-//     origin: ['https://dataforge-platform-c2tj.vercel.app'],
-//     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true,
-//   });
-
-//   const config = new DocumentBuilder()
-//     .setTitle('CRUD Platform API')
-//     .setDescription('Auto-generated CRUD + RBAC Platform')
-//     .setVersion('1.0')
-//     .addBearerAuth()
-//     .build();
-//   const document = SwaggerModule.createDocument(app, config);
-//   SwaggerModule.setup('api', app, document);
-
-//   const port = process.env.PORT || 3001;
-//   await app.listen(port);
-//   console.log(`🚀 Server running on http://localhost:${port}`);
-//   console.log(`📚 API Documentation: http://localhost:${port}/api`);
-// }
-
-// bootstrap();
-
-
-
+// backend/src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-export async function createApp() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
+  
   app.enableCors({
-    origin: ['https://dataforge-platform-c2tj.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: ['https://dataforge-platform-c2tj.vercel.app','http://localhost:3001' ],
     credentials: true,
   });
+ 
+  const config = new DocumentBuilder()
+    .setTitle('CRUD Platform API')
+    .setDescription('Auto-generated CRUD + RBAC Platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-  await app.init();
-  return app;
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  console.log(`🚀 Server running on http://localhost:${port}`);
+  console.log(`📚 API Documentation: http://localhost:${port}/api`);
 }
+
+bootstrap();
